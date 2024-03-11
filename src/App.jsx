c75a0ece1c1997af41db6112f9afab02acabd8bc
M0H1K4N <nejer911@gmail.com>
2024-03-11 19:55:16 +0400

    add seperate variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
-
-
 
 function Header() {
   const description = reactDescriptions[genRandomInt(2)];
@@ -17,9 +15,20 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} React concepts you will need for almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept() {
+  return (
+    <li>
+      <img src="" />
+      <h3>TITLE</h3>
+      <p>Description</p>
+    </li>
   );
 }
 
@@ -28,7 +37,19 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The Core UI building block."
+              img={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
