521a0356130b2e376b757e021fdc1bd1b8e3634f
M0H1K4N <nejer911@gmail.com>
2024-03-11 20:04:01 +0400

    adding props with array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -25,7 +25,7 @@ function Header() {
 function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} />
+      <img src={props.image} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -41,13 +41,25 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The Core UI building block."
-              img={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description} 
+              image={CORE_CONCEPTS[0].image} 
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+          <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description} 
+              image={CORE_CONCEPTS[1].image} 
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description} 
+              image={CORE_CONCEPTS[2].image} 
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description} 
+              image={CORE_CONCEPTS[3].image} 
+            />
           </ul>
         </section>
       </main>
