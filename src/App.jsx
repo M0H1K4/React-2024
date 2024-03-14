aa9c2f6bb82a0a6d54d8b640f59619e297581e04
M0H1K4N <nejer911@gmail.com>
2024-03-14 17:15:33 +0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            
             <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
